77ab612413c80ee1bf144a471807f60d824fa7c0
Ruiz05 <RuizTML11@yahoo.com>
2021-10-07 09:58:05 +0800

    update login admin dan member

--- partials/login_template.php
+++ partials/login_template.php
@@ -10,7 +10,20 @@ if (!defined('INDEX_AUTH')) {
       prefix="og: http://ogp.me/ns#">
 <head>
 <!-- meta -->
-
+<style>
+  .heading1{
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size:1.2rem;
+    
+  }
+  .pig{
+    padding:25px 50px 50px;
+    
+  }
+  #content{
+    background-color: rgb(229, 229, 229);
+  }
+</style>
 </head>
 
 <body itemscope="itemscope" itemtype="http://schema.org/WebPage">
@@ -21,9 +34,10 @@ include 'nav.php';
 <main id="content" class="s-main" role="main">
 <div class="login">
     <div class="container-fluid">
-        <div class="row justify-content-center">
-        <img src="template/slims_perpus/gambar/heder.png" alt="" class="logo_login">
-        <?php echo $main_content;?>
+        <div class="row justify-content-center pig">
+          <div class="col-auto offset-1"><img class="loginpage" src="template/slims_perpus/gambar/logo.png" style="width: 100px; height: auto; padding-top: 50%; padding-bottom: 50%;"></div>
+          <div class="col-6" style="padding-top: 10px;">LOGIN ADMIN
+          <?php echo $main_content;?></div>
         </div>
     </div>
 </div>
--- partials/member.php
+++ partials/member.php
@@ -1,4 +1,48 @@
 <?php if (!($is_login)) : ?>
-          <?php 
-          echo $main_content; ?>
+
+<!DOCTYPE html>
+<html lang="<?php echo substr($sysconf['default_lang'], 0, 2); ?>" xmlns="http://www.w3.org/1999/xhtml"
+      prefix="og: http://ogp.me/ns#">
+<head>
+<!-- meta -->
+<style>
+  .heading1{
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size:1.2rem;
+    
+  }
+  .pig{
+    padding:25px 50px 50px;
+    
+  }
+  #content{
+    background-color: rgb(229, 229, 229);
+  }
+</style>
+</head>
+
+<body itemscope="itemscope" itemtype="http://schema.org/WebPage">
+
+<?php
+include 'nav.php';
+?>
+<main id="content" class="s-main" role="main">
+<div class="login">
+    <div class="container-fluid">
+        <div class="row justify-content-center pig">
+          <div class="col-auto offset-1"><img class="loginpage" src="template/slims_perpus/gambar/logo.png" style="width: 100px; height: auto; padding-top: 50%; padding-bottom: 50%;"></div>
+          <div class="col-6" style="padding-top: 10px;">
+          <?php echo $main_content;?></div>
+        </div>
+    </div>
+</div>
+</main>
+
+
+</body>
+<?php
+//include 'diatas_footer.php';
+include "footer.php";
+?>
+</html>
 <?php endif; ?>
