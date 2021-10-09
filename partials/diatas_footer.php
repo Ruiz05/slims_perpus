94242595ee0e1aa9fe997d7b224ad0cf9448bbcc
Ruiz05 <RuizTML11@yahoo.com>
2021-10-10 07:01:43 +0800

    update gambar path footer

--- partials/diatas_footer.php
+++ partials/diatas_footer.php
@@ -5,32 +5,32 @@
     <div class="row gambar_dibawah">
       <!--masukkan jurnal bari disini-->
       <div class="col col-md col-sm-12">
-      <a href="https://journals.umkt.ac.id/index.php/bsr/sp/" target="_blank"><img src="template/bootstrap4/img/jurnal_1.png" alt="jurnal_1" width="250px;" leght="100px;"></a>
+      <a href="https://journals.umkt.ac.id/index.php/bsr/sp/" target="_blank"><img src="template/slims_perpus/gambar/jurnal_1.png" alt="jurnal_1" width="250px;" leght="100px;"></a>
     </div>
     <div class="col col-md col-sm-12">
-      <a href="https://journals.umkt.ac.id/" target="_blank"><img src="template/bootstrap4/img/jurnal_2.png" alt="jurnal_2" width="250px;" leght="100px;"></a>
+      <a href="https://journals.umkt.ac.id/" target="_blank"><img src="template/slims_perpus/gambar/jurnal_2.png" alt="jurnal_2" width="250px;" leght="100px;"></a>
     </div>
     <div class="col col-md col-sm-12">
-      <a href="https://doaj.org/" target="_blank"><img src="template/bootstrap4/img/jurnal_3.png" alt="jurnal_3" width="250px;" leght="100px;"></a>
+      <a href="https://doaj.org/" target="_blank"><img src="template/slims_perpus/gambar/jurnal_3.png" alt="jurnal_3" width="250px;" leght="100px;"></a>
     </div>
     <div class="col col-md col-sm-12">
-      <a href="https://e-resources.perpusnas.go.id/" target="_blank"><img src="template/bootstrap4/img/jurnal_4.png" alt="jurnal_4" width="250px;" leght="100px;"></a>
+      <a href="https://e-resources.perpusnas.go.id/" target="_blank"><img src="template/slims_perpus/gambar/jurnal_4.png" alt="jurnal_4" width="250px;" leght="100px;"></a>
     </div>
     </div>
     <!--tambah Jurnal baru-->
     <div class="row gambar_dibawah">
       <!--masukkan jurnal bari disini-->
       <div class="col col-md col-sm-12">
-      <a href="https://journals.umkt.ac.id/index.php/bsr/sp/" target="_blank"><img src="template/bootstrap4/img/jurnal_1.png" alt="jurnal_1" width="250px;" leght="100px;"></a>
+      <a href="https://journals.umkt.ac.id/index.php/bsr/sp/" target="_blank"><img src="template/slims_perpus/gambar/jurnal_1.png" alt="jurnal_1" width="250px;" leght="100px;"></a>
     </div>
     <div class="col col-md col-sm-12">
-      <a href="https://journals.umkt.ac.id/" target="_blank"><img src="template/bootstrap4/img/jurnal_2.png" alt="jurnal_2" width="250px;" leght="100px;"></a>
+      <a href="https://journals.umkt.ac.id/" target="_blank"><img src="template/slims_perpus/gambar/jurnal_2.png" alt="jurnal_2" width="250px;" leght="100px;"></a>
     </div>
     <div class="col col-md col-sm-12">
-      <a href="https://doaj.org/" target="_blank"><img src="template/bootstrap4/img/jurnal_3.png" alt="jurnal_3" width="250px;" leght="100px;"></a>
+      <a href="https://doaj.org/" target="_blank"><img src="template/slims_perpus/gambar/jurnal_3.png" alt="jurnal_3" width="250px;" leght="100px;"></a>
     </div>
     <div class="col col-md col-sm-12">
-      <a href="https://e-resources.perpusnas.go.id/" target="_blank"><img src="template/bootstrap4/img/jurnal_4.png" alt="jurnal_4" width="250px;" leght="100px;"></a>
+      <a href="https://e-resources.perpusnas.go.id/" target="_blank"><img src="template/slims_perpus/gambar/jurnal_4.png" alt="jurnal_4" width="250px;" leght="100px;"></a>
     </div>
     </div>
     <!--end colum baru-->
